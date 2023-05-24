4ba5714ec67010e1e10440b0d73ae530249fc39d
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-05-25 00:09:34 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace How_many_times_present_same_array_of_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 1, 1, 2, 2, 5 };
+            int count = 0;
+            if (input[0] == input[1]) 
+            {
+                count++;
+                {
+                    if (input[2] == input[3])
+                    {
+                        count++;
+                    }
+                    Console.Write(" this answer is =" + count + " input[1] and input[2] is two times present");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
+        
+    
+
